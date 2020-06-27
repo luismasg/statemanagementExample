05ef7c453a7ad4d0c384e3a1b4d9220ce76790b4
Luis Suarez <Luis_Suarez@epam.com>
2020-06-27 11:56:27 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useEffect, useState } from 'react';
 
-function App() {
+const Company = ({ name, catchPhrase }) => {
+  useEffect(() => {
+    return () => console.log(`company ${name}  will unmount`);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ border: '1px solid rgba(0,0,0,.2)', borderRadius: 10 }}>
+      Company : {name}
+      location : ioinon
+      <p>{catchPhrase}</p>
     </div>
   );
+};
+
+class Usuario extends Component {
+  state = { showCompany: true };
+  render() {
+    const { name, email, company } = this.props.user;
+    return (
+      <div
+        style={{ fontWeight: 10, width: '%33vw', borderRadius: 5, border: '1px solid blue', margin: 10, padding: 10 }}
+      >
+        <button
+          onClick={() => {
+            this.setState((prevState) => ({ showCompany: !prevState.showCompany }));
+          }}
+        >
+          {this.state.showCompany ? 'Hide Company' : 'show Company'}
+        </button>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <span>{name}</span>
+          <span>{email}</span>
+        </div>
+        {this.state.showCompany && <Company {...company} />}
+      </div>
+    );
+  }
 }
 
-export default App;
+const Demo = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((usersData) => {
+        setUsers(usersData);
+      });
+  }, []);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setUsers((ps) => {
+            return ps.filter((item) => item.id !== 1);
+          });
+        }}
+      >
+        delete
+      </button>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {users.map((user) => (
+          <Usuario user={user} key={user.name} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Demo;
